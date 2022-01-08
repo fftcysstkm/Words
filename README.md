8b0733865673c41154d803412c6d3dc1d9a40efa
fftcysstkm <t.satobm3012@gmail.com>
2022-01-08 22:44:44 +0900

    初回コミット

---
+++ README.md
@@ -0,0 +1,86 @@
+# Words App
+
+This folder contains the source code for the Words app codelab.
+
+# Introduction
+
+Words app allows you to select a letter and use Intents to navigate to an Activity that presents a
+number of words starting with that letter. Each word can be looked up via a web search.
+
+Words app contains a scrollable list of 26 letters A to Z in a RecyclerView. The orientation of the
+RecyclerView can be changed between a vertical list or a grid of items.
+
+The app demonstrates the use of Intents in two ways:
+
+* to navigate inside an app by specifying an explicit destination, and,
+* allowing Android to service the Intent using the apps and resources present on the device.
+
+# Pre-requisites
+
+* Experience with Kotlin syntax.
+* Able to create an Activity.
+* Able to create a RecyclerView and supply it with data.
+
+# Getting Started
+
+1. Install Android Studio, if you don't already have it.
+2. Download the sample.
+3. Import the sample into Android Studio.
+4. Build and run the sample.
+
+# Google公式Kotlinチュートリアル
+
+Intent（明示的・暗黙的）の使い方について。 MainActivityでアルファベットを表示、タップで明示インテントにより詳細画面に移動
+詳細画面ではランダムで単語リストが表示。タップして暗黙的インテントによりブラウザ立ち上げ、単語検索 そのほか、メニューバーに用意したボタンで線形レイアウトorグリッドレイアウトを切り替える。
+
+# 明示インテントのメモ
+
+今回の例では、adapter内のonBindViewHolder()にて、クリックリスナーとして画面起動のイベントを登録。
+
+```kotlin
+// context取得しておく（遷移先アクティビティ起動に必要のため）
+val intent = Intent(context, ExampleActivity::class.java)
+intent.putExtras(キー, 遷移先アクティビティに渡す文字列とか)
+context.startActivity(intent)
+```
+
+キーは、ハードコーディングよりは遷移先Activityにcompanion objectで持たせておくとよい。
+
+# 暗黙的インテントのメモ
+
+`ACTION_VIEW`以外にも色々と汎用インテントがある((
+詳細)[https://developer.android.com/codelabs/basic-android-kotlin-training-activities-intents?hl=ja&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-kotlin-unit-3-pathway-1%3Fhl%3Dja%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-training-activities-intents#6])。
+
+```kotlin
+// context取得しておく（ほかのアプリ起動に必要のため）
+val queryUrl: Uri = Uri.parse("${DetailActivity.SEARCH_PREFIX}${item}")
+val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+context.startActivity(intent)
+```
+
+# RecyclerViewのレイアウトマネージャー切り替えメモ
+
+↓こんな感じでRecyclerViewのlayoutManagerプロパティの設定を変えるとレイアウトが変わる。
+
+```kotlin
+        if (isLinearLayoutManager) {
+    recyclerView.layoutManager = LinearLayoutManager(this)
+} else {
+    recyclerView.layoutManager = GridLayoutManager(this, 4)
+}
+recyclerView.adapter = LetterAdapter()
+```
+
+# メニューバーの使い方メモ
+
+- res/menuフォルダ内にメニュー用レイアウトリソース作成
+- menuタグ内に、メニューに表示したいアイテムをitemタグで用意
+- MainActivityで最低下記のメソッドをオーバーライドする
+
+```kotlin
+//メニューレイアウトファイルをインフレート
+override fun onCreateOptionsMenu(menu: Menu?): Boolean
+
+// メニューのボタンが選択されたときの処理
+override fun onOptionsItemSelected(item: MenuItem): Boolean
+```
---
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    // 現在のレイアウトの状態（true:線形レイアウト、false:グリッドレイアウト）
+    private var isLinearLayoutManager = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        
+        // アダプターとレイアウトマネージャーをリサイクラービューに設定
+        chooseLayout()
+    }
+
+    /**
+     * リサイクラービューのレイアウトマネージャーを切り替える
+     */
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
+        recyclerView.adapter = LetterAdapter()
+    }
+
+    /**
+     * レイアウトマネージャーに応じたアイコン(線形orグリッド)をメニューバーに表示する
+     */
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null) {
+            return
+        }
+        menuItem.icon =
+            if (isLinearLayoutManager) {
+                ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+            } else {
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            }
+    }
+
+    /**
+     * ■メニューバーを使用するのにオーバーライド必須
+     * メニューレイアウトファイルをインフレートする。また、setIcon()呼び出しで適切なアイコンを設定
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        // 適切なアイコンを設定
+        setIcon(layoutButton)
+        return true
+    }
+
+    /**
+     * ■メニューバーを使用するのにオーバーライド必須
+     * メニューのボタンが選択されたときの処理
+     * （chooseLayout()呼び出しでレイアウト変更）
+     * （setIcon()呼び出しでアイコン変更）
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            // レイアウトスイッチのボタンの場合
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+
+            // それ以外の場合、何もしない
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
